937a33d680f531ac83a8fd1693a4e6c2f28bbfe7
cangoksoy1 <130163083+cangoksoy1@users.noreply.github.com>
2024-05-19 13:39:11 +0300

    Update inventorysimulation.py

--- inventorysimulation.py
+++ inventorysimulation.py
@@ -18,9 +18,24 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 if 'button_clicked' not in st.session_state:
     st.session_state.button_clicked = False
 
-if st.button('Press Me', key='press_me_button', position: absolute; top: 300px; left: 50%; transform: translateX(-50%); on_click=lambda: st.session_state.update(button_clicked=True)):
+if st.button('Press Me', key='press_me_button', on_click=lambda: st.session_state.update(button_clicked=True)):
+    press_me_button ="""
+    <style>
+     position: absolute;
+    top: 300px;
+    left: 50%;
+    transform: translateX(-50%);
+    background-color: #000000;
+    color: white;
+    font-size: 24px;
+    padding: 15px 30px;
+    border: none;
+    cursor: pointer;
+}
+</style>
+"""
     st.session_state.button_clicked = True
-
+    
 if st.session_state.button_clicked:
     st.markdown(
         """
