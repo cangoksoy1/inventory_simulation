28dc4e15ce80fc1ffb5b436718d75159264c449b
cangoksoy1 <130163083+cangoksoy1@users.noreply.github.com>
2024-05-19 13:02:39 +0300

    Update inventorysimulation.py

--- inventorysimulation.py
+++ inventorysimulation.py
@@ -26,6 +26,10 @@ if st.session_state.button_clicked:
         """
         <style>
         .modal-content {
+            position: absolute;
+            top:300px;
+            left:50%;
+            transform:translateX(-50%);
             background-color: rgba(0,0,0,0.8);
             color: white;
             padding: 20px;
